d82dd128829145760d0ad8394e553dbfa1a196b8
acoomes33 <github email address>
2020-11-15 19:51:00 -0600

    updated drinks controller

--- mixaholic-backend/app/controllers/drinks_controller.rb
+++ mixaholic-backend/app/controllers/drinks_controller.rb
@@ -1,2 +1,15 @@
 class DrinksController < ApplicationController
+    def index
+        drinks = Drink.all
+        render json: drinks
+    end 
+
+    def show 
+        drink = Drink.find_by(params[:id])
+        render json: drink
+    end 
+
+    def create
+
+    end 
 end
