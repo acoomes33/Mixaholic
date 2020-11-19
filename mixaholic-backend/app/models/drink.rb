1dc824a9337820eb6e9a7c42afcc477617ee2256
acoomes33 <github email address>
2020-11-18 19:31:36 -0600

    updated nested attributes

--- mixaholic-backend/app/models/drink.rb
+++ mixaholic-backend/app/models/drink.rb
@@ -1,5 +1,5 @@
 class Drink < ApplicationRecord
     has_many :ingredients
     has_many :reviews
-    accepts_nested_attributes_for :ingredients, :allow_destroy => true
+accepts_nested_attributes_for :ingredients, :allow_destroy => true, reject_if: proc {|ing| ing['description'].blank?}
 end
